8abfe2ba3ad6ed6fd5425ebfab99a46b267bc1c8
coding <356300546@qq.com>
2020-11-10 11:55:48 +0800

    hx

---
+++ app/Http/Admin/Controllers/AuthController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Admin\Controllers;
+
+use Dcat\Admin\Http\Controllers\AuthController as BaseAuthController;
+
+class AuthController extends BaseAuthController
+{
+    
+}
